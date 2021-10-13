cf1b25376d2835f9f463325c06c3aaceae7955a2
RedAura <86167689+MoeZilla@users.noreply.github.com>
2021-10-13 23:03:23 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,43 @@
+package main 
+
+import (
+        "os"
+	"os/signal"
+	"syscall"
+        "logs"
+	"time"
+
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+func main()  {
+	err := godotenv.LOad(".env")
+	if err != nil {
+		panic("Unable .env file")
+	}
+
+	Token := os.Getenv("token")
+	if Token == "" {
+		panic("enter your bot token")
+	}
+
+	kk, err := discordgo.New("Bot " + token)
+	if err != nil {
+		panic(err)
+	}
+
+	kk.AddHandler(discord.chatbot)
+	
+	err = kk.Open()
+	if err != nil {
+		log.Panic("connected to server")
+	}
+
+	log.Print("Bot is ready")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+	kk.Close()
+}
